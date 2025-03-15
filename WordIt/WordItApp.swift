ab47b0afc59ac3e0f418e2b4b18ce8f9031a7bfc
Sree Sai Raghava <raghava.rym@gmail.com>
2025-03-15 12:37:13 +0530

    Initial Commit

---
+++ WordIt/WordItApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordItApp.swift
+//  WordIt
+//
+//  Created by Sree Sai Raghava Dandu on 15/03/25.
+//
+
+import SwiftUI
+
+@main
+struct WordItApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
